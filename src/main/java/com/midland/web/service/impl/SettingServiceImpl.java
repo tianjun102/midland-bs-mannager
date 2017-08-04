171bd8f9afdee8acc251004fb190f9e04487388f
Administrator <977543176@qq.com>
2017-08-04 15:46:17 +0800

    111

--- src/main/java/com/midland/web/service/impl/SettingServiceImpl.java
+++ src/main/java/com/midland/web/service/impl/SettingServiceImpl.java
@@ -10,10 +10,12 @@ import com.midland.web.model.Area;
 import com.midland.web.model.Popular;
 import com.midland.web.service.SettingService;
 import com.midland.web.util.MidlandHelper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Map;
+
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 @Service
 public class SettingServiceImpl implements SettingService {
@@ -26,7 +28,7 @@ public class SettingServiceImpl implements SettingService {
 
     @Override
     public PageList<Popular> findPopularList(Popular popular, PageBounds pageBounds) {
-        return popularMapper.selectPropularList(popular,pageBounds);
+        return popularMapper.selectPropularList(popular, pageBounds);
     }
 
     @Override
@@ -45,20 +47,104 @@ public class SettingServiceImpl implements SettingService {
     }
 
     @Override
-    public List<Area> queryAreaByRedis(Map<String, String> parem) {
+    public Map<String, List<Area>> queryCityByRedis(Map<String, String> parem) {
+        //先在缓存中查询
+        Map<String, List<Area>> areaMap = this.getArea(parem.get("flag"),parem.get("id"),parem.get("parentId"));
+        //如果缓存查不到再调接口查
+        if (areaMap == null){
 
-       String data = HttpUtils.get(AppSetting.getAppSetting("APIURL"),parem);
-        List<Area>  areaList =  MidlandHelper.getPojoList(data, Area.class);
-        for (Area area : areaList){
-            baseRedisTemplate.addListItem(area.getId(),area.getName());
-            baseRedisTemplate.addListItem(area.getParentId(),area.getParentName());
+            String data = HttpUtils.get(AppSetting.getAppSetting("APIURL"), parem);
+            List<Area> areaList = MidlandHelper.getPojoList(data, Area.class);
+            String parentId = "";
+            for (Area area : areaList) {
+                if (!parentId.equals(area.getParentId())) {
+                    parentId = area.getParentId();
+                    baseRedisTemplate.saveValue("province:" + area.getParentId(), area);
+                }
+                    baseRedisTemplate.saveValue("city:" + area.getId() + ":" + area.getParentId(), area);
+            }
+
+            areaMap = this.getArea(parem.get("flag"),parem.get("id"),parem.get("parentId"));
         }
 
+        return areaMap;
 
-
-        return  null;
     }
 
+    /**
+     *
+     * @param parem flag = province(省) , city(市) ,area(区), sheet(片区) id 或 parentId
+     * @return
+     */
+    @Override
+    public Map<String, List<Area>> queryAreaByRedis(Map<String, String> parem) {
+        //先在缓存中查询
+        Map<String, List<Area>> areaMap = this.getArea(parem.get("flag"),parem.get("cityId"),parem.get("areaId"));
 
+        if (areaMap == null){
+
+            String data = HttpUtils.get(AppSetting.getAppSetting("AREAURL"), parem);
+            List<Area> areaList = MidlandHelper.getPojoList(data, Area.class);
+            String parentId = "";
+            for (Area area : areaList) {
+                if(StringUtils.isNotEmpty(parem.get("cityId")) &&StringUtils.isEmpty (parem.get("areaId"))){
+                    baseRedisTemplate.saveValue("area:" + area.getId() + ":" + parem.get("cityId"), area);
+                }else if (StringUtils.isNotEmpty(parem.get("cityId")) && StringUtils.isNotEmpty(parem.get("areaId"))){
+                    baseRedisTemplate.saveValue("sheet:" + area.getId() + ":" + area.getParentId(), area);
+                }
+            }
+
+            areaMap = this.getArea(parem.get("flag"),parem.get("cityId"),parem.get("areaId"));
+        }
+
+        return areaMap;
+    }
+
+    /**
+     * 根据省份id获取下级市区
+     * @param flag = province(省) , city(市)
+     * @param id
+     * @return
+     */
+    private Map<String, List<Area>> getArea(String flag, String id,String parentId) {
+        Map<String, List<Area>> result = new HashMap<>();
+        List<Area> areaList = new LinkedList<>();
+        Set<byte[]> keys = null;
+        if ("provice".equals(flag)) {
+             keys = baseRedisTemplate.getKeysLike("provice:*");
+             if(keys==null||keys.size()<=0){
+                 return null;
+             }
+        }else if ("city".equals(flag)){
+             keys = baseRedisTemplate.getKeysLike("city:*:"+id);
+            if(keys==null||keys.size()<=0){
+                return null;
+            }
+        }else if("area".equals(flag)){
+            keys = baseRedisTemplate.getKeysLike("area:*:"+id);
+            if(keys==null||keys.size()<=0){
+                return null;
+            }
+        }else if("sheet".equals(flag)){
+            keys = baseRedisTemplate.getKeysLike("sheet:*:"+parentId);
+            if(keys==null||keys.size()<=0){
+                return null;
+            }
+        }
+
+        for (byte[] a : keys) {
+            String key = "";
+            try {
+                key = StringUtils.toString(a, null);
+                Area area = (Area) baseRedisTemplate.getValueByKey(key);
+                areaList.add(area);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        result.put(flag,areaList);
+
+        return  result;
+    }
 
 }
