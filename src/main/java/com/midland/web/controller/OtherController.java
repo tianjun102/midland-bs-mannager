ea681faa2fc17758016f84d05a578060edbdde1c
root <3332932@qq.com>
2017-08-31 15:56:13 +0800

    Merge branch 'master' of https://github.com/tianjun102/midland-bs-mannager
    # Conflicts:
    #	src/main/java/com/midland/web/dao/xml/SpecialPageMapper.xml
    #	src/main/webapp/WEB-INF/views/specialPage/addSpecialPage.jsp
    #	src/main/webapp/WEB-INF/views/specialPage/specialPageList.jsp

--- src/main/java/com/midland/web/controller/OtherController.java
+++ src/main/java/com/midland/web/controller/OtherController.java
@@ -15,36 +15,52 @@ import javax.servlet.http.HttpServletRequest;
  * Created by 'ms.x' on 2017/8/7.
  */
 @Controller
-public class OtherController extends BaseController{
+public class OtherController extends BaseController {
 	@Autowired
 	private FooterService footerServiceImpl;
 	
+	@RequestMapping("/privacy/index")
+	public String privacyIndex(HttpServletRequest request, Model model) {
+		Footer footer = footerServiceImpl.getFooter();
+		model.addAttribute("item", footer);
+		return "footer/privacy";
+	}
+	
+	@RequestMapping("/disclaimer/index")
+	public String disclaimerIndex(HttpServletRequest request, Model model) {
+		Footer footer = footerServiceImpl.getFooter();
+		model.addAttribute("item", footer);
+		return "footer/disclaimer";
+	}
+	
+	@RequestMapping("/registrationProtocol/index")
+	public String registrationProtocolIndex(HttpServletRequest request, Model model) {
+		Footer footer = footerServiceImpl.getFooter();
+		model.addAttribute("item", footer);
+		return "footer/registrationProtocol";
+	}
+	
 	@RequestMapping("/serviceArea/index")
-	public String serviceAreaIndex(HttpServletRequest request,Model model) {
-		Footer footer=footerServiceImpl.getFooter();
-		model.addAttribute("item",footer);
+	public String serviceAreaIndex(HttpServletRequest request, Model model) {
+		Footer footer = footerServiceImpl.getFooter();
+		model.addAttribute("item", footer);
 		return "footer/serviceArea";
 	}
-
+	
 	@RequestMapping("/aboutUs/index")
-	public String aboutUsIndex(HttpServletRequest request,Model model) {
-		Footer footer=footerServiceImpl.getFooter();
-		model.addAttribute("item",footer);
+	public String aboutUsIndex(HttpServletRequest request, Model model) {
+		Footer footer = footerServiceImpl.getFooter();
+		model.addAttribute("item", footer);
 		return "footer/aboutUs";
 	}
-
+	
 	
 	@RequestMapping("/tradingProcess/index")
-	public String tradingProcessIndex(HttpServletRequest request,Model model) {
-		Footer footer=footerServiceImpl.getFooter();
-		model.addAttribute("item",footer);
+	public String tradingProcessIndex(HttpServletRequest request, Model model) {
+		Footer footer = footerServiceImpl.getFooter();
+		model.addAttribute("item", footer);
 		return "footer/tradingProcess";
 	}
-
-	
-	
-	
-	
 	
 	
 }
